a9bcde614be2729ca445f8998212d139093994da
Ismail Mert Karadayi <imertkaradayi@gmail.com>
2023-04-04 15:12:02 +0300

    Engin

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -17,12 +17,15 @@ async function main() {
     "../ethers-simple-storage-fcc/SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
+  // ENGIN
 
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("Deploying, please wait...");
   const contract = await contractFactory.deploy(); // Stop here wait for the contract to deploy.
   await contract.deployTransaction.wait(1);
+  console.log(`Contract Address: ${contract.address}`);
 
+  //Get Number
   const currentFavoriteNumber = await contract.retrieve();
   console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
   const transactionResponse = await contract.store("7");
